d9b1942f4344e461c623e9d61c4455aedd1b425e
ProgrammerShahJalal <you@example.com>
2023-01-23 08:48:40 +0600

    recusion-factorial

--- recursion_part1.cpp
+++ recursion_part1.cpp
@@ -1,7 +1,15 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    
+int factorial(int n)
+{
+   return  n==0 ? 1 : factorial(n - 1) * n;
+ 
+}
+int main()
+{
+    int n;
+    cin >> n;
+    cout << factorial(n) << endl;
     return 0;
 }
